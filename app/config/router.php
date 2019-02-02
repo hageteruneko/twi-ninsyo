c2b7bb95a1e202622b0a22eb52daeb3a8b45b7d4
hageteruneko <39547980+hageteruneko@users.noreply.github.com>
2019-02-03 05:46:31 +0900

    yr

--- app/config/router.php
+++ app/config/router.php
@@ -2,6 +2,41 @@
 
 $router = $di->getRouter();
 
+$router->add(
+    "/login",
+    array(
+        "controller" => "index",
+        "action"     => "login",
+    )
+);
+$router->add(
+    "/callback.phtml",
+    array(
+        "controller" => "index",
+        "action"     => "callback",
+    )
+);
+$router->add(
+    "/products",
+    array(
+        "controller" => "products",
+        "action"     => "index",
+    )
+);
+$router->add(
+    "/logout",
+    array(
+        "controller" => "index",
+        "action"     => "logout",
+    )
+);
+$router->add(
+    "/error.phtml",
+    array(
+        "controller" => "index",
+        "action"     => "error",
+    )
+);
 // Define your routes here
 
 $router->handle();
